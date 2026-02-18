d2a27b9a85c7e4e2125a37dfb2c8b3f25705f412
Kuntal Das <kuntdas787@gmail.com>
2026-02-18 19:39:28 +0530

    add config and base

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+import os
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "Water Quality AI Backend"
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql+psycopg2://postgres:postgres@db:5432/waterdb"
+    )
+    MODEL_PATH: str = os.getenv("MODEL_PATH", "app/ml/models/water_model_v1.pkl")
+    MODEL_VERSION: str = os.getenv("MODEL_VERSION", "v1")
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
